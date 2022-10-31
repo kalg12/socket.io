a34553e80f8a8ac3ac8aef91cfa25da5e8796297
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-10-31 00:25:08 -0600

    Initial server with sockets.io

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const { createServer } = require('http'); // Importamos la función createServer de http module
+const { Server } = require('socket.io'); // Importamos la clase Server de socket.io module
+
+const app = express(); // Creamos una instancia de express
+const httpServer = createServer(app); // Creamos una instancia de httpServer
+
+const io = new Server(httpServer); // Creamos una instancia de io
+
+//Declaramos nuestros archivos estáticos
+app.use(express.static(path.join(__dirname, 'views'))); // Declaramos la carpeta views como estática
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html'); // Enviamos como vista el archivo index.html
+});
+
+io.on('connection', (socket) => { // Escuchamos el evento connection
+    console.log(socket.id); // Imprimimos en consola el mensaje
+});
+
+httpServer.listen(3000); // Escuchamos en el puerto 3000
